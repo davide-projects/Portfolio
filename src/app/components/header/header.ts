2eb93e349f4d3ea7804ef54954f6d568dfd16acf
Davide <davide.barbieri1@outlook.it>
2026-07-21 13:04:07 +0200

    Upgrade: Aggiunto Tema chiaro/scuro

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, signal, Renderer2 } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+  isDark = signal(true);
+
+  constructor(
+    @Inject(DOCUMENT) private readonly _document: Document,
+    @Inject(PLATFORM_ID) private readonly _platformId: object,
+    private readonly _renderer: Renderer2
+  ) {
+    if (isPlatformBrowser(this._platformId)) {
+      const saved = localStorage.getItem('theme');
+      if (saved === 'portfolio-light') {
+        this.isDark.set(false);
+      }
+    }
+  }
+
+  toggleTheme() {
+    this.isDark.update(v => !v);
+    const theme = this.isDark() ? 'portfolio' : 'portfolio-light';
+
+    // ✔️ NESSUN WARNING SONARCUBE
+    this._renderer.setAttribute(
+      this._document.documentElement,
+      'data-theme',
+      theme
+    );
+
+    if (isPlatformBrowser(this._platformId)) {
+      localStorage.setItem('theme', theme);
+    }
+  }
+}
